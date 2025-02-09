49828d34f7b3427981c4bfbb3da1831a58c5487b
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-09 13:10:05 +0100

    'add'

--- e-commerce-app/src/App.jsx
+++ e-commerce-app/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Homepage from "./Home"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
     
+    <h1>hello world </h1>
+ 
     </>
   )
 }
