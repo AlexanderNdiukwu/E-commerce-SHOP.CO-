9c76ef40d8d53c115527a1c6bf5b21428db7fd72
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-10 16:57:49 +0100

    add'

--- e-commerce-app/src/NavBar/Navbar.jsx
+++ e-commerce-app/src/NavBar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className='grid mx-24 py-2.5'>
-      <div className='grid grid-cols-4 items-center bg text-sm'>
+    <div className='grid lg:mx-24 py-2.5'>
+      <div className='grid  lg:grid-cols-4 items-center bg text-sm'>
         <div className='grid grid-cols-2 lg:grid-cols-1'>
           <p className='block md:hidden'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
@@ -19,7 +19,7 @@ const Navbar = () => {
           <p className='text-[32px] font-bold whitespace-nowrap text-center'>SHOP.CO</p>
         </div>
 
-        <div className=' md:grid grid-cols-4 hidden   '>
+        <div className=' lg:grid grid-cols-4 hidden   '>
           <div className='relative'>
             <button onClick={toggleDropdown} className='focus:outline-none'>
               Shop
@@ -65,14 +65,14 @@ const Navbar = () => {
 
         <div className='grid grid-cols-2 gap-2 '>
           <div>
-            <p className='grid justify-end'>
+            <p className='grid lg:justify-end'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
             </p>
           </div>
 
-          <div className='grid justify-start'>
+          <div className='grid lg:justify-start'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
