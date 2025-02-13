f8504df4f013b05704190e8c1397f79c85339352
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-13 13:12:26 +0100

    'add'

--- e-commerce-app/src/HomePage/TopSell.jsx
+++ e-commerce-app/src/HomePage/TopSell.jsx
@@ -2,7 +2,13 @@ import Card from "../CardComponent.jsx/Card";
 import View from "../ViewButton/View";
 
 const TopSell = () => {
-    
+    let fetchedData = [
+        // { imageFit: 'w-full', ProductImage: '/images/CardImages/image.png', desc: 't-shirt', rating: '/images/CardImages/starRate.png', price: "$20", OriginalPrice: 'ddd ', discount: 'e' },
+        // { imageFit: 'w-full', ProductImage: '/images/CardImages/image.png', desc: 't-shirt', rating: '/images/CardImages/starRate.png', price: "$20", OriginalPrice: 'ddd ', discount: 'e' },
+        // { imageFit: 'w-full', ProductImage: '/images/CardImages/image.png', desc: 'jeans', rating: '/images/CardImages/starRate.png', price: "$20" },
+        // { imageFit: 'w-full', ProductImage: '/images/CardImages/image.png', desc: 't-shirt', rating: '/images/CardImages/starRate.png', price: "$20", OriginalPrice: 'ddd ', discount: 'e' },
+        
+    ];
 
 
 
@@ -16,15 +22,38 @@ const TopSell = () => {
                     TOP SELL 
                 </div>
 
+                {fetchedData.length  > 0 ? 
+
                 <div className="grid grid-cols-4 gap-5">
 
-                    <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-                    <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='jeans' rating='/images/CardImages/starRate.png' price="$20" />
-                    <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-                    <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-                
+                    {fetchedData.map((item , index )=> 
+
+                        <Card 
+                        key={index}
+                        imageFit={item.imageFit}
+                        ProductImage={item.ProductImage}
+                        desc={item.desc}
+                        rating={item.rating}
+                        price={item.price}
+                        OriginalPrice={item.OriginalPrice}
+                        discount={item.discount}
+                        />
+                    
+
+
+                    )}
 
                 </div>
+
+                :
+                                 <div className="text-center text-gray-500 font-bold  animate-bounce shadow-2xl">No new arrivals at the moment.</div>
+
+
+
+
+
+                }
+
                 <div className="grid justify-center my-10">
                     <View/>
                 </div>
