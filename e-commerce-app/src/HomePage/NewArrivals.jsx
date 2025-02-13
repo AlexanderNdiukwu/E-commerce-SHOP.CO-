5ce5da52ffbef925cb296f23f4c10e525e9134c1
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-13 12:55:08 +0100

    'add'

--- e-commerce-app/src/HomePage/NewArrivals.jsx
+++ e-commerce-app/src/HomePage/NewArrivals.jsx
@@ -5,8 +5,10 @@ const NewArrivals = () => {
 
 
     let fetchedData = [
-        {}
-
+        {imageFit:'w-full', ProductImage:'/images/CardImages/image.png', desc:'t-shirt' ,rating:'/images/CardImages/starRate.png', price:"$20" ,OriginalPrice:'ddd ', discount:'e'},
+        {imageFit:'w-full', ProductImage:'/images/CardImages/image.png', desc:'t-shirt' ,rating:'/images/CardImages/starRate.png' ,price:"$20" ,OriginalPrice:'ddd ' ,discount:'e'},
+        {imageFit:'w-full' ,ProductImage:'/images/CardImages/image.png' ,desc:'jeans' ,rating:'/images/CardImages/starRate.png', price:"$20"},
+        {imageFit:'w-full', ProductImage:'/images/CardImages/image.png' ,desc:'t-shirt', rating:'/images/CardImages/starRate.png', price:"$20", OriginalPrice:'ddd ' ,discount:'e'},
 
     ]
 
@@ -28,12 +30,12 @@ const NewArrivals = () => {
         </div>
 
         <div className="grid grid-cols-4 gap-5">
-
+        {if fetchedData.length > }
+            <Card />
+            {/* <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='jeans' rating='/images/CardImages/starRate.png' price="$20" />
             <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-            <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='jeans' rating='/images/CardImages/starRate.png' price="$20" />
             <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-            <Card imageFit='w-full' ProductImage='/images/CardImages/image.png' desc='t-shirt' rating='/images/CardImages/starRate.png' price="$20" OriginalPrice='ddd ' discount='e'/>
-         
+          */}
 
         </div>
 
