c22c431dd5b81e6a690fb16f3b10c003704e6e7d
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-13 15:54:10 +0100

    'add'

--- e-commerce-app/src/HomePage/HeaderDetails.jsx
+++ e-commerce-app/src/HomePage/HeaderDetails.jsx
@@ -17,12 +17,12 @@ const HeaderDetails = () => {
 
     return (  
 
-        <div className="grid grid-cols-2 h-[70vh] lg:mx-24   ">
+        <div className="grid lg:grid-cols-2 h-[70vh] lg:mx-24   ">
 
 
 
             <div className="grid items-center my-10">
-                <div className="text-6xl font-bold">
+                <div className=" text-4xl lg:text-6xl font-bold">
                     FIND CLOTHES THAT MATCHES YOUR STYLE
                 </div>
 
@@ -60,7 +60,7 @@ const HeaderDetails = () => {
     <div className="relative overflow-hidden w-full  h-full">
         <img src="/images/FooterImages/bigBing.png" alt="bigBing" className=" absolute z-30 left-96 top-3"/>
         <img src="/images/FooterImages/smallBing.png" alt="smallBing"  className=" absolute z-30 top-96" />
-        <img src="/images/MainHeadPhoto.png" alt="headMain" className="object-fit  h-full relative right-48 bottom-4 " />
+        <img src="/images/MainHeadPhoto.png" alt="headMain" className="object-fit  h-full lg:relative right-48 bottom-4 " />
     </div>
 
 </div>
