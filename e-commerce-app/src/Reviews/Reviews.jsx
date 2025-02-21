cc9e8d5f9cab3f7a8a7d0999fe915d0e5101af86
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-21 16:05:32 +0100

    'add'

--- e-commerce-app/src/Reviews/Reviews.jsx
+++ e-commerce-app/src/Reviews/Reviews.jsx
@@ -1,64 +1,51 @@
-
-import ReviewCard from "../CardComponent.jsx/ReviewCard";
+import { useRef } from "react";
 import ReviewHeader from "./ReviewHeader";
+import ReviewCard from "./ReviewCard";
 
 const Reviews = () => {
+    const reviewContainerRef = useRef(null);
 
-    let reviewData = [
+    const reviewData = [
+        { rating: '/images/CardImages/starRate.png', name: 'sarahsmd,kskksksckididih', comment: 'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm' },
+        { rating: '/images/CardImages/starRate.png', name: 'sarahsmd,kskksksckididih', comment: 'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm' },
+        { rating: '/images/CardImages/starRate.png', name: 'sarahsmd,kskksksckididih', comment: 'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm' },
+        { rating: '/images/CardImages/starRate.png', name: 'sarahsmd,kskksksckididih', comment: 'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm' },
+        { rating: '/images/CardImages/starRate.png', name: 'sarahsmd,kskksksckididih', comment: 'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm' },
+    ];
 
-        {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
-        {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
-        {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
-        {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
-        {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
-        
-    ]
+    const scrollLeft = () => {
+        reviewContainerRef.current.scrollBy({
+            left: -300,
+            behavior: 'smooth'
+        });
+    };
 
+    const scrollRight = () => {
+        reviewContainerRef.current.scrollBy({
+            left: 300,
+            behavior: 'smooth'
+        });
+    };
 
-
-
-
-
-    return ( <div className="lg:mx-24 mx-3 py-4">
-
-
-        <ReviewHeader/>
-        {reviewData.length > 0 ? 
-
-            <div className="grid lg:grid-cols-4 grid-flow-col w-full overflow-hidden border  gap-2">
-
-            {reviewData.map((item,index) => 
-
-                    <ReviewCard
-                    key={index}
-                    rating={item.rating}
-                    name={item.name}
-                    comment={item.comment}
-
-
-                    />
-
-
-
-
-)}
-
+    return (
+        <div className="lg:mx-24 mx-3 py-4">
+            <ReviewHeader scrollLeft={scrollLeft} scrollRight={scrollRight} />
+            {reviewData.length > 0 ? (
+                <div ref={reviewContainerRef} className="grid lg:grid-cols-4 grid-flow-col w-full overflow-x-auto border gap-2">
+                    {reviewData.map((item, index) => (
+                        <ReviewCard
+                            key={index}
+                            rating={item.rating}
+                            name={item.name}
+                            comment={item.comment}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div className="text-center text-gray-500 font-bold animate-bounce shadow-2xl">NO COMMENTS YET.</div>
+            )}
         </div>
-:
+    );
+};
 
-<div className="text-center text-gray-500 font-bold  animate-bounce shadow-2xl">NO COMMENTS YET .</div>
-
-
-}
-
-
-
-          
-
-
-      
-
-    </div> );
-}
- 
 export default Reviews;
