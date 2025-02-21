619db7e964ba51969a134d9f50eb1ec673d6b8bf
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-02-21 16:15:54 +0100

    'add'

--- e-commerce-app/src/Reviews/ReviewHeader.jsx
+++ e-commerce-app/src/Reviews/ReviewHeader.jsx
@@ -1,27 +1,33 @@
-const ReviewHeader = ({ scrollLeft, scrollRight }) => {
-    return (
-        <div className="my-10">
-            <div>
-                <div className="grid grid-cols-3">
-                    <div className="col-span-2">
-                        <p className="font-bold text-4xl lg:text-5xl">OUR HAPPY CUSTOMER</p>
-                    </div>
-                    <div className="flex gap-4 justify-end items-end">
-                        <button onClick={scrollLeft}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                        <button onClick={scrollRight}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
+const ReviewHeader = () => {
+    return (  <div className=" my-10 ">
+        <div>
+
+            <div className="grid grid-cols-3">
+                <div className="col-span-2">
+                <p className="font-bold text-4xl lg:text-5xl">OUR HAPPY CUSTOMER </p>
+
+                </div>
+
+                <div className="flex gap-4 justify-end items-end">
+                    <p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+</svg>
+
+</p>
+
+<p>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+</svg>
+
+
+</p>
+
                 </div>
             </div>
-        </div>
-    );
-};
 
+        </div>
+    </div> );
+}
+ 
 export default ReviewHeader;
