2016a3fc1892c11248c08119f3f7198dec543b48
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-03 00:04:04 +0100

    'add'

--- e-commerce-app/src/RoutePages/CategoryMain.jsx
+++ e-commerce-app/src/RoutePages/CategoryMain.jsx
@@ -1,9 +1,29 @@
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import {motion,AnimatePresence} from 'framer-motion'
 import CategoryClotheFilterMain from "../CategoryaPage/CategoryClotheFilterMain";
 import CategoryMainFilter from "../CategoryaPage/CategoryMainFilter";
 import MainForm from "../EmailForm/MainForm";
 import MainFooter from "../Footer/MainFooter";
+
+
+let meunbar ={
+  hidden:{
+    opacity:0,y:1000
+  },
+  visible:{
+    opacity:1,y:0,
+    transition:{duration:0.5 ,ease:'easeIn'}
+  },
+
+  exit:{
+    y:1000
+  }
+}
+
+
+
+
 
 const CategoryMain = () => {
     const [isFilterOpen, setIsFilterOpen] = useState(false);
@@ -21,22 +41,41 @@ const CategoryMain = () => {
    
     };
 
+
+
+
+
+
     
     return ( <div>
 
          <div className=" relative flex  flex-col-reverse lg:flex-row gap-5 border-t-2 border-[#0000001A] lg:mx-24  lg:py-5">
-         <div
-          className={`order-1 h-full rounded-3xl transition-all duration-500 ease-in-out transform lg:relative absolute inset-x-0 bottom-0 lg:inset-auto bg-white z-30 ${
-            isFilterOpen
-              ? 'translate-y-0 opacity-100'
-              : `hidden lg:block`
-          }`}
-        >
-        
-               <CategoryMainFilter toggleFilter={toggleFilter} />
 
-              
-            </div>
+          <AnimatePresence>
+            { isFilterOpen && 
+            
+            <motion.div
+            variants={meunbar}
+            initial='hidden'
+            animate='visible'
+      
+            exit='exit'
+              className={`order-1 h-full rounded-3xl transition-all duration-500 ease-in-out transform lg:relative absolute inset-x-0 bottom-0 lg:inset-auto bg-white z-30 
+              `}
+            >
+            
+                  <CategoryMainFilter toggleFilter={toggleFilter} />
+
+                  
+                </motion.div>
+                
+            
+            }
+
+
+            </AnimatePresence>
+
+
             <div className='order-2 mx-3 '>
                 <CategoryClotheFilterMain  toggleFilter={toggleFilter}/>
             </div>
