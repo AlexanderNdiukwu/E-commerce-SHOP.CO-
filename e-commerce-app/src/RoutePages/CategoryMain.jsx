366751e1883392528ca03edc8bdb0d4c74019e75
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-03 19:45:02 +0100

    'add'

--- e-commerce-app/src/RoutePages/CategoryMain.jsx
+++ e-commerce-app/src/RoutePages/CategoryMain.jsx
@@ -9,7 +9,7 @@ import MainFooter from "../Footer/MainFooter";
 
 let meunbar ={
   hidden:{
-    opacity:0,y:1000
+    opacity:0,y:'100vh'
   },
   visible:{
     opacity:1,y:0,
@@ -36,7 +36,7 @@ const CategoryMain = () => {
     const toggleFilter = () => {
 
         setIsFilterOpen(!isFilterOpen);
-        console.log('the btn is being clicked')
+     
 
     };
 
@@ -51,20 +51,31 @@ const CategoryMain = () => {
          <div className=" relative flex  flex-col-reverse lg:flex-row gap-5 border-t-2 border-[#0000001A] lg:mx-24  lg:py-5">
 
           <AnimatePresence>
-           
-            
-            <motion.div
-              variants={meunbar}
+           {isFilterOpen &&   <motion.div
+                  variants={meunbar}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
             
               className={`order-1 h-full rounded-3xl transition-all duration-500 ease-in-out transform lg:relative absolute inset-x-0 bottom-0 lg:inset-auto bg-white z-30 
-            ${isFilterOpen ? `${hidden='hidden'}` : `${exit='exit'}`}`}
+            `}
             >
                   <CategoryMainFilter toggleFilter={toggleFilter} />
     
                 </motion.div>
-                        
+                     }
+            
+              
          
             </AnimatePresence>
+            <div
+              
+              className={`order-1 hidden lg:block h-full rounded-3xl transition-all duration-500 ease-in-out transform lg:relative absolute inset-x-0 bottom-0 lg:inset-auto bg-white z-30 
+            `}
+            >
+                  <CategoryMainFilter toggleFilter={toggleFilter} />
+    
+                </div>
 
 
             <div className='order-2 mx-3 '>
