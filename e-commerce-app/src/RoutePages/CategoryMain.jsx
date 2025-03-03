95bd2d864c63afa067497df276194ce9631187eb
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-03 19:16:05 +0100

    'add'

--- e-commerce-app/src/RoutePages/CategoryMain.jsx
+++ e-commerce-app/src/RoutePages/CategoryMain.jsx
@@ -17,7 +17,9 @@ let meunbar ={
   },
 
   exit:{
-    y:1000
+    y:1000,
+    opacity:0,
+    transition:{ease:'easeIn'}
   }
 }
 
@@ -34,6 +36,7 @@ const CategoryMain = () => {
     const toggleFilter = () => {
 
         setIsFilterOpen(!isFilterOpen);
+        console.log('the btn is being clicked')
 
     };
 
@@ -48,22 +51,19 @@ const CategoryMain = () => {
          <div className=" relative flex  flex-col-reverse lg:flex-row gap-5 border-t-2 border-[#0000001A] lg:mx-24  lg:py-5">
 
           <AnimatePresence>
-            { isFilterOpen && 
+           
             
             <motion.div
-            variants={meunbar}
-            initial='hidden'
-            animate='visible'
-      
-            exit='exit'
+              variants={meunbar}
+            
               className={`order-1 h-full rounded-3xl transition-all duration-500 ease-in-out transform lg:relative absolute inset-x-0 bottom-0 lg:inset-auto bg-white z-30 
-              `}
+            ${isFilterOpen ? `${hidden='hidden'}` : `${exit='exit'}`}`}
             >
                   <CategoryMainFilter toggleFilter={toggleFilter} />
     
                 </motion.div>
                         
-            }
+         
             </AnimatePresence>
 
 
