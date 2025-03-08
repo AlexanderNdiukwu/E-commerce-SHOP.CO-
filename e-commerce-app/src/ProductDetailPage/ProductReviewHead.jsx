88b7e84276574802faea0e94f1e474599de6ac59
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-08 13:56:02 +0100

    'add'

--- e-commerce-app/src/ProductDetailPage/ProductReviewHead.jsx
+++ e-commerce-app/src/ProductDetailPage/ProductReviewHead.jsx
@@ -1,15 +1,37 @@
-const ProductreviewHead = () => {
+import { useState } from "react";
+
+const ProductreviewHead = ({handleVisiblity}) => {
+    let [clicked ,setClicked ] = useState()
+
+    let lists = [
+        {items: 'Product Details'},
+        {items : 'Rating & Reviews'},
+        {items : 'FAQs'},
+    ]
+
+
+  let u = (x)=> {
+    setClicked(x)
+    handleVisiblity(x)
+
+  }
+
+
+
+
     return ( <div>
         <div className=" pt-8 lg:text-xl text-base grid grid-cols-3 font-normal  text-center">
-            <div className="border-b-2 text-[#00000099] border-[#F0F0F0] py-3 lg:py-3">
-                <p className="whitespace-nowrap">Product Details</p>
-            </div>
-            <div className="border-b-2 text-[#000000] border-[#000000] py-3  lg:py-3">
-                <p className="whitespace-nowrap">Rating & Reviews</p>
-            </div>
-            <div className="border-b-2 text-[#00000099] border-[#F0F0F0] py-3  lg:py-3">
-                <p className="whitespace-nowrap">FAQs</p>
-            </div>
+
+            {
+                lists.map((list,index ) => (
+                    <div onClick={()=>u(list.items)} key={index} className={`border-b-2 text-[#00000099] border-[#F0F0F0] py-3 lg:py-3 ${clicked == list.items ? 'border-b-2 text-[#000000] border-black':''}`}>
+                    <p  className="whitespace-nowrap">{list.items}</p>
+                    </div>
+                ))
+            }
+
+
+          
         </div>
     </div> );
 }
--- e-commerce-app/src/Reviews/Reviews.jsx
+++ e-commerce-app/src/Reviews/Reviews.jsx
@@ -1,9 +1,9 @@
-import { useRef } from "react";
+
 import ReviewCard from "../CardComponent.jsx/ReviewCard";
 import ReviewHeader from "./ReviewHeader";
 
 const Reviews = () => {
-    const scrollRef = useRef(null);
+
 
     let reviewData = [
 
@@ -18,35 +18,22 @@ const Reviews = () => {
         {rating:'/images/CardImages/starRate.png', name:'sarahsmd,kskksksckididih', comment:'loremmmmmmmmkefnkesnfksnedvnsdfjsenfjsebfuies fifweifiwef fjwefiuhweifhweifhwef fwjiefbwufwfniuwe wejijdawidwhidhwhd dbwdjwdw dwuidwudhw duwdwuduw w dfw idhw d wd w d w dfw fwefbuwbiufbwuimmm'},
         
     ]
-    const scrollLeft = () => {
-        scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
-        console.log('it is working ')
-      };
-      
-      const scrollRight = () => {
-        console.log ('it is working')
-        scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
-      };
-      
-
-
-
-
+  
 
     return ( <div className="lg:mx-24 mx-3 py-4">
 
 
-        <ReviewHeader scrollLeft={scrollLeft} scrollRight={scrollRight}/>
+        <ReviewHeader/>
 
-        <div ref={scrollRef} className= " relative w-full   ">
+        <div  className= " relative w-full   ">
 
 
         {reviewData.length > 0 ? 
 
-            <div className="ref={scrollRef} flex overflow-x-scroll scroll-smooth  gap-2 lg:gap-4">
+            <div className=" flex overflow-x-scroll scroll-smooth  gap-2 lg:gap-4">
                 
             {reviewData.map((item,index) => 
-            <div  key={index} className="lg:min-w-1/4 min-w-full p-4">
+            <div  key={index} className="lg:min-w-1/4 min-w-full py-2">
 
 
                     <ReviewCard
